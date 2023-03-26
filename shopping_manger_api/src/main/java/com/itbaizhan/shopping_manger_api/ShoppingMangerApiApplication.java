e471b540609726b5e7f433c4df948a68a49120a4
2370514218@qq.com <2370514218@qq.com>
2023-03-26 08:27:59 +0800

    创建商品服务模块，根据id查询平白功能 创建网站后台API模块

---
+++ shopping_manger_api/src/main/java/com/itbaizhan/shopping_manger_api/ShoppingMangerApiApplication.java
@@ -0,0 +1,13 @@
+package com.itbaizhan.shopping_manger_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMangerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMangerApiApplication.class, args);
+    }
+
+}
